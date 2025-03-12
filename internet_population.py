06588f825a0d1010670d3c30bffc7fac8c8d1e81
LauCarrillo21 <laucarrillo21@gmail.com>
2025-03-12 11:16:53 -0600

    Connecting data with plot

--- internet_population.py
+++ internet_population.py
@@ -14,29 +14,32 @@ with open(filename, 'r') as csvfile:
   reader = csv.DictReader(csvfile)
   #Loop through each line in reader
   for line in reader:
+    #Convert data to integer to be able to show in plot
     continent = line['continent']
-    year = line['year']
-    population = line['population']
+    year = int(line['year'])
+    population =int(line['population'])
 
     #Check if the continent exists in the dictionary
     if continent not in population_per_continent:
       #Initialize an entry for the new continent
       population_per_continent[continent] = {'population': [], 'years': []}
     #Store the population and year data
+    #The population and year from the current CSV row are added to the respective lists for that continent
     population_per_continent[continent]['population'].append(population)
     population_per_continent[continent]['years'].append(year)
 
-   
-
-#Independent Plot
-#X horizontal axis, Y vertical axis
-plt.plot([2000, 2010, 2020, 2030], [100, 200, 300, 400], label="Europe", marker="o")
-plt.plot([2000, 2010, 2020, 2030], [200, 300, 500, 800], label="Asia", marker="o")
+#Loop through each continet
+for continent in population_per_continent:
+  #Define X and Y variables
+  years = population_per_continent[continent]['years']
+  population = population_per_continent[continent]['population']
+  #Plot with X & Y axis
+  plt.plot(years, population, label=continent, marker="o", alpha=0.5)   
 
 #Plot Details
 plt.title("Internet Population per continent")
 plt.xlabel("Year")
-plt.ylabel("Internet users")
+plt.ylabel("Internet users (in billion users)")
 plt.grid(True)
 plt.legend()
 
