96fed1df3748fb9337efe94c26db7678670b43ed
LauCarrillo21 <laucarrillo21@gmail.com>
2025-03-12 10:53:49 -0600

    Independent plot data

--- internet_population.py
+++ internet_population.py
@@ -1,11 +1,16 @@
 #Import Data
 import csv
+#Import Matplot to be able to create the plot
+import matplotlib.pyplot as plt
+
+#Create a variable to identify the file
 filename = 'data.csv'
 
 #Actions to take with the data
 with open(filename, 'r') as csvfile:
   #Convert file to a Python Dictionary
   reader = csv.DictReader(csvfile)
+  #Loop through each line in reader
   for line in reader:
     continent = line['continent']
     year = line['year']
@@ -14,3 +19,19 @@ with open(filename, 'r') as csvfile:
     print(continent)
     print(year)
     print(population)
+
+
+#Independent Plot
+#X horizontal axis, Y vertical axis
+plt.plot([2000, 2010, 2020, 2030], [100, 200, 300, 400], label="Europe", marker="o")
+plt.plot([2000, 2010, 2020, 2030], [200, 300, 500, 800], label="Asia", marker="o")
+
+#Plot Details
+plt.title("Internet Population per continent")
+plt.xlabel("Year")
+plt.ylabel("Internet users")
+plt.grid(True)
+plt.legend()
+
+#Show the Plot
+plt.show()
